cabb58f76a0dfa49104ac234770c5fff0c816f56
guohua <zhu@guohuadeMacBook-Pro.local>
2017-09-18 21:59:46 +0800

    edit dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,6 +1,8 @@
-var merge = require('webpack-merge')
-var prodEnv = require('./prod.env')
+var merge = require('webpack-merge');
+var prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"'
+  NODE_ENV: '"development"',
+  BASE_API: '"https://demo.g9zz.com"',
+  APP_ORIGIN: '"https://demo.g9zz.com"'
 })
