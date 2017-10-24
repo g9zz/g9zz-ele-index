2cd52a7a5452e2a2822a7d30a6f40b4c7fee8c48
yela528 <2067930913@qq.com>
2017-10-24 21:58:39 +0800

    :ok_hand: 大部分功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,8 @@
 import Router from 'vue-router'
-import Hello from '../components/my/Hello'
-import Test from '../page/test/test.vue'
-import Home from '../components/Home.vue'
-import Test2 from '../page/test/Test2.vue'
 import Index from '../page/post/Index.vue'
 import Show from '../page/post/Show.vue'
 import Create from '../page/post/Create.vue'
-import My from '../page/my/index.vue'
+import My1 from '../page/my/index.vue'
 import Login from '../page/auth/Login.vue'
 import Register from '../page/auth/Register.vue'
 import myCookie from '../utils/cookie.js'
@@ -18,7 +14,7 @@ const routes = [
   { path: '/', name: '首页', component: Index,},
   { path: '/post/:hid', name: '详情', component: Show},
   { path: '/post_create', name: '创建', meta: { auth:true }, component: Create},
-  { path: '/my', name: '个人中心', meta: { auth:true }, component: My},
+  { path: '/my', name: '个人中心', meta: { auth:true }, component: My1},
   { path: '/node', name: '节点列表', component: NodeIndex },
   { path: '/node/:hid/post', name: '节点列表', component: PostListWithNode },
   { path: '/login', name: '登录', component: Login },
