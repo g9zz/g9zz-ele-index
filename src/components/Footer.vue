bbe6185ccd5021f9509126e985019f06e12c5eff
zghua <xzghua@gmail.com>
2020-01-10 16:01:59 +0800

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="beian">
     ©2017 G9ZZ.  <a href="https://github.com/g9zz" target="_blank"> 关于我们</a>
-    <a href="http://www.beian.miit.gov.cn/" target="_blank">皖ICP备15017682号-2</a>
+    <a href="http://www.beian.miit.gov.cn/" target="_blank">皖ICP备15017682号</a>
   </div>
 </template>
 
